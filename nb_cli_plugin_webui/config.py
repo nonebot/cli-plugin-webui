7b5dc6c6cf35562f20f2da5cb9ce2e9eb0633580
Kyomotoi <kyomotoiowo@gmail.com>
2023-05-19 02:49:17 +0800

    :art: 完善设置类

--- nb_cli_plugin_webui/config.py
+++ nb_cli_plugin_webui/config.py
@@ -1,21 +1,25 @@
+from pathlib import Path
+
 from nb_cli_plugin_webui import PLUGIN_NAME
+from nb_cli_plugin_webui.models.app.config import WebUIConfig
 from nb_cli_plugin_webui.utils.localstore import get_config_file
-from nb_cli_plugin_webui.models.schemas.config import WebUIConfig
+
+CONFIG_PATH = get_config_file(PLUGIN_NAME, "webui-config.json")
 
 
 class Config:
-    CONFIG_PATH = get_config_file(PLUGIN_NAME, "webui-config.json")
+    def __init__(self, config_file: Path):
+        self.config_file = config_file
 
-    def __init__(self, conf: WebUIConfig):
-        self.conf = conf
+    @property
+    def exist(self) -> bool:
+        return CONFIG_PATH.is_file()
 
-    @classmethod
-    def exist(cls) -> bool:
-        return cls.CONFIG_PATH.is_file()
+    def read(self) -> WebUIConfig:
+        return WebUIConfig.parse_file(self.config_file)
 
-    @classmethod
-    def load(cls) -> WebUIConfig:
-        return WebUIConfig.parse_file(cls.CONFIG_PATH, encoding="utf-8")
+    def store(self, data: WebUIConfig) -> None:
+        CONFIG_PATH.write_text(data.to_json(), encoding="utf-8")
 
-    def store(self) -> None:
-        self.CONFIG_PATH.write_text(self.conf.to_json(), encoding="utf-8")
+
+config = Config(CONFIG_PATH)
