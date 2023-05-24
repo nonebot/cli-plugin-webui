ff21c7054e12981bc8120b2d42b9c276eafdbb75
Kyomotoi <kyomotoiowo@gmail.com>
2023-05-25 00:12:59 +0800

    :art: 完善前端主体代码

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "@/style.css";
+import App from "@/App.vue";
+import { registerPlugins } from "@/plugins";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+registerPlugins(app);
+
+app.mount("#app");
