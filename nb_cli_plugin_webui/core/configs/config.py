67e2917ccaf8bba6a78d7d3bff822a70016429e2
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-06 14:43:46 +0800

    :truck: 移动 config 至 core

--- nb_cli_plugin_webui/core/configs/config.py
+++ nb_cli_plugin_webui/core/configs/config.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from typing import Union
 
 from nb_cli_plugin_webui import PLUGIN_NAME
 from nb_cli_plugin_webui.models.app.config import WebUIConfig
 from nb_cli_plugin_webui.utils.localstore import get_config_file
 
 CONFIG_PATH = get_config_file(PLUGIN_NAME, "webui-config.json")
+CONFIG_CACHE: Union[WebUIConfig, None] = None
 
 
 class Config:
@@ -15,8 +17,14 @@ class Config:
     def exist(self) -> bool:
         return CONFIG_PATH.is_file()
 
-    def read(self) -> WebUIConfig:
-        return WebUIConfig.parse_file(self.config_file)
+    def read(self, read_local: bool = False) -> WebUIConfig:
+        if read_local:
+            return WebUIConfig.parse_file(self.config_file)
+
+        global CONFIG_CACHE
+        if not CONFIG_CACHE:
+            CONFIG_CACHE = WebUIConfig.parse_file(self.config_file)
+        return CONFIG_CACHE
 
     def store(self, data: WebUIConfig) -> None:
         CONFIG_PATH.write_text(data.to_json(), encoding="utf-8")
