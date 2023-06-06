90880b32fe9634649560f3931bbdb9d72327eb8b
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-06 14:56:37 +0800

    :sparkles: 新增密钥检查

--- nb_cli_plugin_webui/api/routes/authentication.py
+++ nb_cli_plugin_webui/api/routes/authentication.py
@@ -1,24 +1,27 @@
-from fastapi import Body, APIRouter, HTTPException, status
+from fastapi import Body, Depends, APIRouter, HTTPException, status
 
-from nb_cli_plugin_webui.core.config import config
 from nb_cli_plugin_webui.utils.security import jwt
-from nb_cli_plugin_webui.models.schemas.user import (
-    UserInLogin,
-    UserWithJWT,
-    UserInResponse,
-)
+from nb_cli_plugin_webui.core.configs.config import config
+from nb_cli_plugin_webui.models.schemas.user import UserInLogin, UserInResponse
+from nb_cli_plugin_webui.models.schemas.authentication import IsAvailableResponse
+from nb_cli_plugin_webui.api.dependencies.authentication import get_current_header
 
 router = APIRouter()
 
 
 @router.post("/login", response_model=UserInResponse)
 async def login(
-    login_data: UserInLogin = Body(..., embed=True),
+    login_data: UserInLogin = Body(embed=True),
 ) -> UserInResponse:
     if not login_data.check_token():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
     jwt_token = jwt.create_access_for_header(
-        login_data.string, config.read().secret_key.get_secret_value()
+        login_data.mark, config.read().secret_key.get_secret_value()
     )
-    return UserInResponse(user=UserWithJWT(jwt_token=jwt_token))
+    return UserInResponse(jwt_token=jwt_token)
+
+
+@router.get("/is_available", response_model=IsAvailableResponse)
+async def is_alive(_=Depends(get_current_header)) -> IsAvailableResponse:
+    return IsAvailableResponse(detail="0w0")
