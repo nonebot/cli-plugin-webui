47351ff89824017c1a412e4d219763f1d43df1f8
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-06 14:59:46 +0800

    :sparkles: 添加文件操作类 API 支持

--- nb_cli_plugin_webui/api/routes/api.py
+++ nb_cli_plugin_webui/api/routes/api.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
 
-from nb_cli_plugin_webui.api.routes import driver, adapter, performance, authentication
+from nb_cli_plugin_webui.api.routes import (
+    check,
+    files,
+    driver,
+    adapter,
+    performance,
+    authentication,
+)
 
 router = APIRouter()
 router.include_router(authentication.router, tags=["authentication"], prefix="/auth")
 router.include_router(performance.router, tags=["performance"], prefix="/performance")
 router.include_router(adapter.router, tags=["adapter"], prefix="/adapter")
 router.include_router(driver.router, tags=["driver"], prefix="/driver")
+router.include_router(check.router, tags=["check"], prefix="/check")
+router.include_router(files.router, tags=["files"], prefix="/files")
