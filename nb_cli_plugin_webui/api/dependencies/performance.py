8f7b9cc2779f5aafb0e3ef36b7952025e5255e26
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-13 15:50:23 +0800

    :art: 同步 models 重构及优化代码

--- nb_cli_plugin_webui/api/dependencies/performance.py
+++ nb_cli_plugin_webui/api/dependencies/performance.py
@@ -1,9 +1,9 @@
-from nb_cli_plugin_webui.models.app.performance import SystemStats
-from nb_cli_plugin_webui.utils.performance import PlatformPerformanceMonitor
+from nb_cli_plugin_webui.utils.performance import PerformanceMonitor
+from nb_cli_plugin_webui.models.schemas.performance import SystemStats
 
 
 def get_system_stats() -> SystemStats:
-    data = PlatformPerformanceMonitor()
+    data = PerformanceMonitor()
     return SystemStats(
         platform=data.get_platform_info(),
         cpu=data.get_cpu_info(),
