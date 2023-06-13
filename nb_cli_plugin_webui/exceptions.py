733077251a05db747a1b47018d7d0f05119db37a
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-13 15:38:46 +0800

    :sparkles: 添加异常类型

--- nb_cli_plugin_webui/exceptions.py
+++ nb_cli_plugin_webui/exceptions.py
@@ -3,4 +3,28 @@ class TokenComplexityError(Exception):
 
 
 class ConfigIsNotExist(Exception):
-    """config is not exist."""
+    """config file is not exist."""
+
+
+class ProcessAlreadyRunning(Exception):
+    """target process already running."""
+
+
+class ProcessAlreadyExist(Exception):
+    """target process already exist."""
+
+
+class LoggerStorageAlreadyExist(Exception):
+    """target log storage already exist."""
+
+
+class FunctionAlreadyExist(Exception):
+    """target function already exist."""
+
+
+class NonebotProjectFileIsNotExist(Exception):
+    """nonebot projects info file is not exist."""
+
+
+class NonebotProjectIsNotExist(Exception):
+    """target nonebot project is not exist."""
