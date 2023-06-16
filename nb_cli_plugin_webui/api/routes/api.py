0cdb03e8181bc17f46975bc8a724b337293a9aa7
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-16 16:50:11 +0800

    :sparkles: 添加新的接口路由

--- nb_cli_plugin_webui/api/routes/api.py
+++ nb_cli_plugin_webui/api/routes/api.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from nb_cli_plugin_webui.api.routes import (
+    log,
     check,
     files,
     driver,
     adapter,
+    project,
     performance,
     authentication,
 )
@@ -13,6 +15,8 @@ router = APIRouter()
 router.include_router(authentication.router, tags=["authentication"], prefix="/auth")
 router.include_router(performance.router, tags=["performance"], prefix="/performance")
 router.include_router(adapter.router, tags=["adapter"], prefix="/adapter")
+router.include_router(project.router, tags=["project"], prefix="/project")
 router.include_router(driver.router, tags=["driver"], prefix="/driver")
 router.include_router(check.router, tags=["check"], prefix="/check")
 router.include_router(files.router, tags=["files"], prefix="/files")
+router.include_router(log.router, tags=["log"], prefix="/log")
