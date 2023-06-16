e3f69c67bb8f1184f44daac7ce23450e763263e3
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-17 00:23:27 +0800

    :fire: 移除中间件 WebSocket 验证部分

--- nb_cli_plugin_webui/api/dependencies/authentication.py
+++ nb_cli_plugin_webui/api/dependencies/authentication.py
@@ -1,12 +1,10 @@
-import asyncio
 from typing import List, Optional
 
 from fastapi import status
-from starlette.requests import Request
+from starlette.types import ASGIApp
 from starlette.responses import Response
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.types import Send, Scope, ASGIApp, Receive
 from fastapi.security.utils import get_authorization_scheme_param
 
 from nb_cli_plugin_webui.utils.security import jwt
@@ -22,41 +20,6 @@ class CustomAuthMiddleware(BaseHTTPMiddleware):
         super().__init__(app)
         self.pass_paths = pass_paths or list()
 
-    async def _ws_close(
-        self, send: Send, code: int = 1000, reason: Optional[str] = None
-    ) -> None:
-        if reason is None:
-            reason = str()
-        await send({"type": "websocket.close", "code": code, "reason": reason})
-
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if scope["type"] == "websocket":
-            await receive()
-            try:
-                recv = await asyncio.wait_for(receive(), 5)
-            except asyncio.TimeoutError:
-                await self._ws_close(send, 1008)
-                return
-
-            token = recv.get("text", "unknown")
-            try:
-                jwt.verify_and_read_jwt(
-                    token, config.read().secret_key.get_secret_value()
-                )
-            except Exception:
-                await self._ws_close(send, 1008)
-                return
-        else:
-            await self.app(scope, receive, send)
-
-    def get_current_header(
-        self,
-        request: Request,
-    ) -> None:
-        authorization = request.headers.get("Authorization")
-        _, param = get_authorization_scheme_param(authorization)
-        jwt.verify_and_read_jwt(param, config.read().secret_key.get_secret_value())
-
     async def dispatch(self, request, call_next) -> Response:
         path = request.url.path
         for p in self.pass_paths:
@@ -64,8 +27,10 @@ class CustomAuthMiddleware(BaseHTTPMiddleware):
                 response = await call_next(request)
                 return response
 
+        authorization = request.headers.get("Authorization")
+        _, param = get_authorization_scheme_param(authorization)
         try:
-            self.get_current_header(request)
+            jwt.verify_and_read_jwt(param, config.read().secret_key.get_secret_value())
             response = await call_next(request)
             return response
         except Exception as err:
