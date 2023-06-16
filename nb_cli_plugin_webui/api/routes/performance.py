8cb9f8f5952f20f30559b3a6ab3ddd960ab40652
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-17 00:25:52 +0800

    :art: 同步中间件的更新, 优化代码

--- nb_cli_plugin_webui/api/routes/performance.py
+++ nb_cli_plugin_webui/api/routes/performance.py
@@ -1,10 +1,10 @@
 import asyncio
 
 from fastapi import Depends, APIRouter
-from fastapi.websockets import WebSocket, WebSocketState
+from fastapi.websockets import WebSocketState
 
+from nb_cli_plugin_webui.patch import WebSocket
 from nb_cli_plugin_webui.api.dependencies.performance import get_system_stats
-from nb_cli_plugin_webui.api.dependencies.authentication import ws_validate_key
 from nb_cli_plugin_webui.models.schemas.performance import (
     SystemStats,
     SystemStatsResponse,
@@ -15,7 +15,8 @@ router = APIRouter()
 
 @router.websocket("/ws")
 async def _(websocket: WebSocket, data: SystemStats = Depends(get_system_stats)):
-    await ws_validate_key(websocket)
+    await websocket.accept()
+
     try:
         while websocket.client_state == WebSocketState.CONNECTED:
             await websocket.send_json(SystemStatsResponse(system_stats=data).dict())
