ecbd47a96380d0a23b96df05c12590e0efe480c6
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-17 00:27:47 +0800

    :art: 整合后端事件处理

--- nb_cli_plugin_webui/api/__init__.py
+++ nb_cli_plugin_webui/api/__init__.py
@@ -2,13 +2,11 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from nb_cli_plugin_webui.core.configs.config import config
+from nb_cli_plugin_webui.api.event import add_event_handler
 from nb_cli_plugin_webui.exceptions import ConfigIsNotExist
+from nb_cli_plugin_webui.api.error import add_exception_handler
 from nb_cli_plugin_webui.api.routes.api import router as api_router
 from nb_cli_plugin_webui.api.dependencies.authentication import CustomAuthMiddleware
-from nb_cli_plugin_webui.api.event import (
-    create_stop_app_handler,
-    create_start_app_handler,
-)
 
 
 def get_application() -> FastAPI:
@@ -17,6 +15,7 @@ def get_application() -> FastAPI:
 
     conf = config.read()
     app = FastAPI(**conf.server.fastapi_kwargs)
+    app.add_middleware(CustomAuthMiddleware, pass_paths=["/api/auth"])
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -24,10 +23,9 @@ def get_application() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    app.add_middleware(CustomAuthMiddleware, pass_paths=["/api/auth"])
 
-    app.add_event_handler("startup", create_start_app_handler)
-    app.add_event_handler("shutdown", create_stop_app_handler)
+    app = add_event_handler(app)
+    app = add_exception_handler(app)
 
     app.include_router(api_router, prefix="/api")
 
--- nb_cli_plugin_webui/api/event.py
+++ nb_cli_plugin_webui/api/event.py
@@ -1,6 +1,14 @@
 from typing import Callable
 
+from fastapi import FastAPI
+
 from nb_cli_plugin_webui.utils.apscheduler import scheduler
+
+
+def add_event_handler(app: FastAPI) -> FastAPI:
+    app.add_event_handler("startup", create_start_app_handler)
+    app.add_event_handler("shutdown", create_stop_app_handler)
+    return app
 
 
 def create_start_app_handler() -> Callable:
