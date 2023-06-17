f82f16d82526bacab724232cdb8b16d5dd0b995b
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-17 15:44:30 +0800

    :sparkles: 添加适用于全局的信息提示

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,8 +3,13 @@ import "@/style.css";
 import App from "@/App.vue";
 import { registerPlugins } from "@/plugins";
 
+import "@/api/authenticationInterceptor";
+import { ToastWrapper } from "./core/notification";
+
 const app = createApp(App);
 
 registerPlugins(app);
 
 app.mount("#app");
+
+export const globalLog = new ToastWrapper("WebUI");
