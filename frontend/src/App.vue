66f57d2101966b21e2cba76f7477fc742f2232f2
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-17 15:51:34 +0800

    :art: 优化页面组成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,5 @@
-<script setup lang="ts">
-import AppSideNav from "@/layouts/AppSideNav.vue";
-import AppStatusBar from "@/layouts/AppStatusBar.vue";
-</script>
-
 <template>
-  <div class="webui" style="height: 100vh">
-    <!-- <AppTopBar /> -->
-    <div class="content">
-      <AppSideNav />
-      <div class="view">
-        <router-view />
-      </div>
-    </div>
-    <AppStatusBar />
-  </div>
+  <router-view />
 </template>
 
 <style>
@@ -21,28 +7,5 @@ import AppStatusBar from "@/layouts/AppStatusBar.vue";
 
 [data-theme="light"] .view {
   background: #fffdfd;
-}
-
-.view {
-  width: 100%;
-  padding: 30px;
-}
-
-@media screen and (max-width: 1280px) {
-  .view {
-    padding: 10px;
-  }
-}
-
-.webui {
-  overflow: hidden;
-
-  background: hsl(var(--b1));
-}
-
-.content {
-  height: calc(100% - 22px);
-  width: 100%;
-  display: flex;
 }
 </style>
