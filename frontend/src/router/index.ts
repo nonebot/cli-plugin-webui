c3de742b6c3dfab1be930cca589d4e44a898852e
Kyomotoi <kyomotoiowo@gmail.com>
2023-06-17 15:39:52 +0800

    :sparkles: 更新路由, 并添加访问规则

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,22 +1,57 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import IndexPage from "@/components/IndexPage.vue";
+import LoginPage from "@/components/LoginPage.vue";
+import IndexView from "@/components/IndexView.vue";
+import HomePage from "@/components/HomePage.vue";
 import PluginManage from "@/components/PluginManage.vue";
+
+import { checkTokenValidity } from "@/core/authentication/authUtils";
+import { globalLog as log } from "@/main";
+import { globalStore } from "@/store/app";
 
 const routes = [
   {
-    path: "/",
-    component: IndexPage,
+    name: "Login",
+    path: "/login",
+    component: LoginPage,
   },
   {
-    path: "/plugin",
-    component: PluginManage,
+    name: "WebUI",
+    path: "/",
+    component: IndexView,
+    children: [
+      {
+        name: "Home",
+        path: "/",
+        component: HomePage,
+      },
+      {
+        name: "Plugin",
+        path: "/plugin",
+        component: PluginManage,
+      },
+    ],
   },
 ];
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(),
   routes,
 });
 
-export default router;
+router.beforeEach(async (to, from) => {
+  const login = await checkTokenValidity();
+  if (!login && to.name !== "Login") {
+    log.warning("请先验证");
+    router.push("/login");
+  }
+
+  if (
+    globalStore().choiceProjectID === "" &&
+    to.path !== "/" &&
+    to.path !== "/login"
+  ) {
+    log.warning("请先选择一项实例");
+    return false;
+  }
+});
