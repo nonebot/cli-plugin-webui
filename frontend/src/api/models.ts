02c8f075f4aa969e6408554d83228cfd88bf7bc3
Kyomotoi <kyomotoiowo@gmail.com>
2023-08-29 22:33:15 +0800

    :sparkles: 更新 API

--- frontend/src/api/models.ts
+++ frontend/src/api/models.ts
@@ -1,12 +1,3 @@
-interface LoginData {
-  token: string;
-  mark: string;
-}
-
-export interface LoginRequest {
-  login_data: LoginData;
-}
-
 export interface LoginResponse {
   jwt_token: string;
 }
@@ -15,33 +6,33 @@ export interface IsAvailableResponse {
   detail: string;
 }
 
-export interface AdaptersResponse {
-  adapters: Adapter[];
+interface Tag {
+  label: string;
+  color: string;
 }
 
-export interface Adapter {
-  name: string;
+export interface SimpleInfo {
   module_name: string;
   project_link: string;
-  desc: string;
-}
-
-export interface DriversResponse {
-  drivers: Driver[];
-}
-
-export interface Driver {
   name: string;
-  module_name: string;
-  project_link: string;
   desc: string;
+  author: string;
+  homepage: string;
+  tags?: Tag[];
+  is_official: boolean;
+  is_download: boolean;
 }
 
-export interface FileMeta {
-  name: string;
-  is_dir: number;
-  path: string;
+export interface Plugin extends SimpleInfo {
+  type?: string;
+  supported_adapters?: string[];
+  valid: boolean;
+  time: string;
 }
+
+export interface Adapter extends SimpleInfo {}
+
+export interface Driver extends SimpleInfo {}
 
 export interface FileDetails {
   name: string;
@@ -49,10 +40,6 @@ export interface FileDetails {
   path: string;
   modified_time: string;
   absolute_path: string;
-}
-
-export interface FileOperateRequest {
-  file_data: FileMeta;
 }
 
 export interface FileInfo {
@@ -67,36 +54,30 @@ export interface FileListResponse {
 }
 
 export interface CreateProjectData {
+  is_bootstrap: boolean;
   project_name: string;
   project_dir: string;
   mirror_url: string;
-  driver: Driver;
-  adapter: Adapter;
-}
-
-export interface CreateProjectRequest {
-  project_data: CreateProjectData;
+  drivers: Driver[];
+  adapters: Adapter[];
+  use_src: boolean;
 }
 
 export interface CreateProjectResponse {
   log_key: string;
 }
 
-interface SimpleInfo {
-  name: string;
-  module_name: string;
-}
-
 export interface NonebotProjectMeta {
   project_id: string;
   project_name: string;
   project_dir: string;
-  mirror: string;
+  mirror_url: string;
   adapters: SimpleInfo[];
   drivers: SimpleInfo[];
-  plugins: string[];
+  plugins: Plugin[];
   plugin_dirs: string[];
   builtin_plugins: string[];
+  is_running: boolean;
 }
 
 export interface NonebotProjectListResponse {
@@ -107,4 +88,63 @@ export interface NonebotProjectListResponse {
 
 export interface DeleteProjectResponse {
   project_id: string;
+}
+
+export interface ProcessLog {
+  time: string;
+  level: string;
+  message: string;
+}
+
+interface ProcessPerformance {
+  cpu: number;
+  mem: number;
+}
+
+export interface ProcessInfo {
+  status_code?: number;
+  total_logs: number;
+  is_running: boolean;
+  performance?: ProcessPerformance;
+}
+
+export interface LogHistoryResponse {
+  detail: ProcessLog[];
+}
+
+export interface StoreListResponse {
+  now_page: number;
+  total_page: number;
+  total_item: number;
+  data: Plugin[] | Adapter[] | Driver[];
+}
+
+export interface InstallModuleResponse extends CreateProjectResponse {}
+
+export interface UninstallModuleResponse extends CreateProjectResponse {}
+
+interface ModuleConfigSimpleInfo {
+  title: string;
+  description: string;
+  name: string;
+}
+
+export interface ModuleConfigChild extends ModuleConfigSimpleInfo {
+  default: any;
+  item_type: string;
+  enum: string[] | number[];
+  configured: any;
+  latest_change: string;
+}
+
+export interface ModuleConfigFather extends ModuleConfigSimpleInfo {
+  properties: ModuleConfigChild[];
+}
+
+export interface ModuleConfigResponse {
+  detail: ModuleConfigFather[];
+}
+
+export interface DotenvListResponse {
+  detail: string[];
 }
