99bef8a66bb313fb37fd271603fd38add3d68e2a
Kyomotoi <kyomotoiowo@gmail.com>
2023-08-29 22:00:48 +0800

    :bug: 同库更改而更改

--- nb_cli_plugin_webui/api/dependencies/performance.py
+++ nb_cli_plugin_webui/api/dependencies/performance.py
@@ -2,11 +2,11 @@ from nb_cli_plugin_webui.utils.performance import PerformanceMonitor
 from nb_cli_plugin_webui.models.schemas.performance import SystemStats
 
 
-def get_system_stats() -> SystemStats:
+async def get_system_stats() -> SystemStats:
     data = PerformanceMonitor()
     return SystemStats(
         platform=data.get_platform_info(),
-        cpu=data.get_cpu_info(),
+        cpu=await data.get_cpu_info(),
         mem=data.get_mem_info(),
         disk=data.get_disk_info(),
         net=data.get_net_info(),
