07e5a944467cf9facd7bb50214a0b4c1942378be
Kyomotoi <kyomotoiowo@gmail.com>
2023-09-09 01:23:20 +0800

    :fire: 移除拓展详细内的项目 README 读取及解析

--- frontend/src/components/NonebotStore/ExtensionDetailModal.vue
+++ frontend/src/components/NonebotStore/ExtensionDetailModal.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import OfficialCheckIcon from "../Icons/OfficialCheckIcon.vue";
 import CheckCircleIcon from "../Icons/CheckCircleIcon.vue";
+import CancelCircleIcon from "../Icons/CancelCircleIcon.vue";
 
 import { Adapter, Driver, Plugin } from "@/api/models";
-import { computed, ref, watch } from "vue";
-import CancelCircleIcon from "../Icons/CancelCircleIcon.vue";
+import { computed, ref } from "vue";
 import { routerTo } from "@/router/client";
+import { API } from "@/api";
+import { appStore } from "@/store/global";
+import { nonebotExtensionStore } from "@/store/extensionStore";
 
 const showModal = ref(false);
 
@@ -22,63 +25,34 @@ defineExpose({
   closeModal,
 });
 
-const cdnList = [
-  "https://cdn.jsdelivr.net/gh/",
-  "https://cdn.staticaly.com/gh/",
-  "https://jsd.cdn.zzko.cn/gh/",
-];
+const api = new API();
 
 const props = defineProps<{ itemData: Plugin | Adapter | Driver }>();
 const itemData = computed(() => {
   return props.itemData;
 });
-const readmeContent = ref("");
 
-const loadReadmeFromCDN = async () => {
-  const branches = ["master", "main"];
-  const urls: string[] = [];
-  for (const cdn in cdnList) {
-    for (const branch in branches) {
-      urls.push(
-        `${cdnList[cdn]}${props.itemData.author}/${props.itemData.module_name}@${branches[branch]}/README.md`,
-      );
-    }
+const uninstallModule = async () => {
+  try {
+    await api.uninstallModule(
+      appStore().choiceProject.project_id,
+      appStore().enabledEnv,
+      props.itemData,
+    );
+    closeModal();
+  } catch (error: any) {
+    console.log(error);
   }
 
-  const loadReadme = async (url: string) => {
-    const resp = await fetch(url);
-    if (resp.ok) {
-      readmeContent.value = await resp.text();
-    } else {
-      throw new Error(`Load README failed from ${url}`);
-    }
-  };
-
-  for (const url in urls) {
-    try {
-      await loadReadme(urls[url]);
-      if (readmeContent.value) {
-        break;
-      }
-    } catch (error: any) {
-      console.log(error);
-    }
-  }
+  try {
+    await nonebotExtensionStore().updateData(appStore().choiceProject.project_id);
+  } catch (error: any) {}
 };
-
-watch(showModal, () => {
-  if (showModal.value) {
-    loadReadmeFromCDN();
-  }
-});
 </script>
 
 <template>
   <dialog :class="{ 'modal pl-0 md:pl-14': true, 'modal-open': showModal }">
-    <form
-      method="dialog"
-      class="modal-box w-11/12 max-w-5xl sm:max-w-4xl rounded-lg"
-    >
+    <form method="dialog" class="modal-box w-11/12 max-w-5xl sm:max-w-4xl rounded-lg">
       <div class="flex items-center">
         <h2 class="font-bold text-xl mr-2">{{ itemData.name }}</h2>
         <OfficialCheckIcon
@@ -86,11 +60,13 @@ watch(showModal, () => {
           class="mr-1 h-6 w-6 text-green-600"
         />
 
-        <CheckCircleIcon
-          v-if="(itemData as Plugin).valid"
-          class="h-6 w-6 text-green-600"
-        />
-        <CancelCircleIcon v-else class="h-6 w-6 text-green-600" />
+        <div v-if="(itemData as Plugin).valid">
+          <CheckCircleIcon
+            v-if="(itemData as Plugin).valid"
+            class="h-6 w-6 text-green-600"
+          />
+          <CancelCircleIcon v-else class="h-6 w-6 text-red-600" />
+        </div>
       </div>
 
       <div class="mt-2 flex">
@@ -100,17 +76,16 @@ watch(showModal, () => {
           class="link link-hover hover:link-primary"
           :href="itemData.homepage"
           target="_blank"
-          >主页</a
         >
+          主页
+        </a>
       </div>
 
       <div class="mt-2">{{ itemData.desc }}</div>
 
       <div class="mt-2 flex gap-2">
-        <button class="btn btn-sm rounded" @click="routerTo('/setting')">
-          设置
-        </button>
-        <button class="btn btn-sm rounded">卸载</button>
+        <button class="btn btn-sm rounded" @click="routerTo('/setting')">设置</button>
+        <button class="btn btn-sm rounded" @click="uninstallModule()">卸载</button>
         <a
           class="btn btn-sm rounded"
           :href="`https://registry.nonebot.dev/plugin/${itemData.project_link}:${itemData.module_name}`"
