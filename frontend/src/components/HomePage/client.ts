95456e2be18072582a3ad4b68c31da5863398449
Kyomotoi <kyomotoiowo@gmail.com>
2023-09-14 17:13:40 +0800

    :art: 整合可能重复用到的代码

--- frontend/src/components/HomePage/client.ts
+++ frontend/src/components/HomePage/client.ts
@@ -2,6 +2,27 @@ import { WebUIWebSocket } from "@/utils/ws";
 import { SystemStatsData } from "@/components/HomePage/models";
 import { systemStatStore } from "@/store/status";
 import { notice } from "@/utils/notification";
+import { API } from "@/api";
+import { appStore } from "@/store/global";
+import { AxiosError } from "axios";
+
+const api = new API();
+
+export const mirrorList = [
+  { name: "PyPI", url: "https://pypi.org/simple" },
+  {
+    name: "校园网联合镜像站",
+    url: "https://mirrors.cernet.edu.cn/pypi/web/simple",
+  },
+  { name: "清华大学", url: "https://pypi.tuna.tsinghua.edu.cn/simple" },
+  {
+    name: "北京外国语大学",
+    url: "https://mirrors.bfsu.edu.cn/pypi/web/simple",
+  },
+  { name: "腾讯云", url: "https://mirrors.cloud.tencent.com/pypi/simple" },
+  { name: "中科院", url: "https://pypi.mirrors.ustc.edu.cn/simple" },
+  { name: "豆瓣", url: "https://pypi.douban.com/simple" },
+];
 
 export function handlePlatformPerformanceWebsocket() {
   const store = systemStatStore();
@@ -60,4 +81,21 @@ export function handlePlatformPerformanceWebsocket() {
     updateList(store.netSentSpeedList, convertToKbps(netSpeeds[0]));
     updateList(store.netRecvSpeedList, convertToKbps(netSpeeds[1]));
   };
+}
+
+export async function getProjectList() {
+  await api
+    .getProjectList()
+    .then((resp) => {
+      appStore().projectList = resp.projects;
+    })
+    .catch((error: AxiosError) => {
+      if (error.response) {
+        notice.error(
+          `获取实例列表失败：${(error.response.data as { detail: string })?.detail}`,
+        );
+      } else {
+        notice.error(`获取实例列表失败：${error.message}`);
+      }
+    });
 }
