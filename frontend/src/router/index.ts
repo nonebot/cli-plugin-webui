4bdc0769cca7e5327a321508cb7bec3c06e9cdd9
Kyomotoi <kyomotoiowo@gmail.com>
2023-09-21 17:38:04 +0800

    :sparkles: 更改 router history 为 hash

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import LoginPage from "@/components/LoginPage.vue";
 import IndexView from "@/components/IndexView.vue";
@@ -47,7 +47,7 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
