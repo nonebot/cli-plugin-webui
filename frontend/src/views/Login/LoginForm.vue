f8b079eb2c7c66cfe00422481412951f0f0da51c
Kyomotoi <kyomotoiowo@gmail.com>
2024-09-06 09:04:35 +0800

    :children_crossing: 优化登录页

--- frontend/src/views/Login/LoginForm.vue
+++ frontend/src/views/Login/LoginForm.vue
@@ -2,6 +2,9 @@
 import { ref } from 'vue'
 import { OpenAPI, AuthService } from '@/client/api'
 import router from '@/router'
+import { useToastStore } from '@/stores/ToastStorage'
+
+const store = useToastStore()
 
 const token = ref(''),
   isDebug = ref(false),
@@ -21,44 +24,49 @@ const login = () => {
     .then((res) => {
       localStorage.setItem('token', res.detail)
       router.push('/')
+      store.add('success', '登录成功', '', 3000)
     })
     .catch((err) => {
-      console.error(err)
+      if (err.body) {
+        store.add('error', err.body.detail)
+        return
+      }
+      store.add('error', '无法连接到服务器', '', 5000)
     })
 }
 </script>
 
 <template>
-  <div class="shrink-0 w-full flex flex-col gap-4">
-    <form class="flex justify-center gap-4 flex-col lg:flex-row" @submit.prevent="login">
-      <label class="form-control">
-        <input
-          v-model="token"
-          type="password"
-          placeholder="请键入登陆凭证"
-          class="input input-ghost bg-base-200"
-          required
-        />
-        <div class="label">
-          <span class="label-text">开发模式</span>
+  <div class="shrink-0 w-full">
+    <form class="flex justify-center flex-col" @submit.prevent="login">
+      <div class="flex justify-center gap-4 flex-col lg:flex-row">
+        <label class="form-control">
           <input
-            type="checkbox"
-            class="checkbox checkbox-xs"
-            :checked="isDebug"
-            @click="isDebug = !isDebug"
+            v-model="token"
+            type="password"
+            placeholder="请键入登陆凭证"
+            class="input input-ghost bg-base-200"
+            required
           />
+          <div class="label">
+            <span class="label-text">开发模式</span>
+            <input
+              type="checkbox"
+              class="checkbox checkbox-xs"
+              :checked="isDebug"
+              @click="isDebug = !isDebug"
+            />
+          </div>
+        </label>
+
+        <div class="form-control">
+          <button class="btn btn-primary text-white">
+            开始使用 <span class="material-symbols-outlined"> chevron_right </span>
+          </button>
         </div>
-      </label>
-
-      <div class="form-control">
-        <button class="btn btn-primary text-white">
-          开始使用 <span class="material-symbols-outlined"> chevron_right </span>
-        </button>
       </div>
-    </form>
 
-    <form v-if="isDebug" class="flex gap-4 flex-col">
-      <div class="form-control">
+      <div v-if="isDebug" class="form-control flex gap-4 flex-col">
         <input
           v-model="host"
           type="text"
@@ -66,9 +74,7 @@ const login = () => {
           class="input input-ghost bg-base-200"
           required
         />
-      </div>
 
-      <div class="form-control">
         <input
           v-model="port"
           type="text"
