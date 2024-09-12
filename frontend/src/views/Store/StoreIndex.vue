f70adad7c679ba6e557ab6b124125a429e5fbcda
Kyomotoi <kyomotoiowo@gmail.com>
2024-09-12 10:50:32 +0800

    :sparkles: 新增商店提示

--- frontend/src/views/Store/StoreIndex.vue
+++ frontend/src/views/Store/StoreIndex.vue
@@ -4,13 +4,18 @@ import ExtensionContainer from './ExtensionContainer.vue'
 import Pagination from './Pagination.vue'
 import { useSearchStore } from './client'
 import { onMounted } from 'vue'
-import { useNoneBotStore } from '@/stores'
+import { useNoneBotStore, useToastStore } from '@/stores'
 
 const store = useSearchStore(),
   nonebotStore = useNoneBotStore()
 
+const toast = useToastStore()
+
 onMounted(async () => {
-  if (!nonebotStore.selectedBot) return
+  if (!nonebotStore.selectedBot) {
+    toast.add('warning', '未选择实例', '', 5000)
+    return
+  }
   await store.updateData(nonebotStore.selectedBot.project_id, false)
 })
 </script>
