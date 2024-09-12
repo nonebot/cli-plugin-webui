69869a6f3780ce317b60447d06c38c80b7badecf
Kyomotoi <kyomotoiowo@gmail.com>
2024-09-12 10:53:39 +0800

    :sparkles: 更新顶部状态栏

--- frontend/src/components/header/Status.vue
+++ frontend/src/components/header/Status.vue
@@ -8,7 +8,8 @@ const customStore = useCustomStore()
 <template>
   <div class="badge-items flex items-center gap-2">
     <div v-if="customStore.isDebug" class="badge badge-warning">开发模式</div>
-    <div class="badge badge-ghost">当前实例: {{ store.selectedBot?.project_name }}</div>
+    <div class="badge badge-ghost">当前实例: {{ store.selectedBot?.project_name || '未选择' }}</div>
+    <div class="badge badge-ghost">当前环境: {{ store.enabledEnv }}</div>
   </div>
 </template>
 
