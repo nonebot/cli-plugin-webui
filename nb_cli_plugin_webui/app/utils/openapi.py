da374b4fd5074e5af05074ba7c4e1eebba4a2cba
Kyomotoi <kyomotoiowo@gmail.com>
2024-10-24 15:31:13 +0800

    :bug: 修复 openapi refer 无法处理 array 的问题

--- nb_cli_plugin_webui/app/utils/openapi.py
+++ nb_cli_plugin_webui/app/utils/openapi.py
@@ -1,13 +1,19 @@
-def resolve_references(schema) -> dict:
+from typing import Any, Dict
+
+
+def resolve_references(schema) -> Dict[str, Any]:
     definitions = schema.get("$defs", {})
 
-    def replace_refs(obj) -> dict:
+    def replace_refs(obj) -> Dict[str, Any]:
         if isinstance(obj, dict):
             if "$ref" in obj:
                 ref = obj["$ref"].split("/")[-1]
                 return definitions[ref]
 
             return {k: replace_refs(v) for k, v in obj.items()}
+
+        if isinstance(obj, list):
+            return {str(i): replace_refs(item) for i, item in enumerate(obj)}
 
         return obj
 
